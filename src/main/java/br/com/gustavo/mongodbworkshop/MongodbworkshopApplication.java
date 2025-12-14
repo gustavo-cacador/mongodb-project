2cff6ad166669607d515062383e080be150096c6
Gustavo <gustavoirl456@outlook.com>
2025-12-14 16:10:24 -0300

    Projeto criado

---
+++ src/main/java/br/com/gustavo/mongodbworkshop/MongodbworkshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.mongodbworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/mongodbworkshop/MongodbworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.mongodbworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
